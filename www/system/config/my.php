468c8716b94d68434ecb616e9f5958a9b3ba9741
linxun <linxunsdut@gmail.com>
2015-01-17 22:01:26 -0500

    pumen init

---
+++ www/system/config/my.php
@@ -0,0 +1,10 @@
+<?php
+$config->installed    = true;	
+$config->debug        = false;	
+$config->requestType  = 'GET';	
+$config->db->host     = 'localhost';	
+$config->db->port     = '3306';	
+$config->db->name     = 'www_permaculture_org_cn';	
+$config->db->user     = 'linxun';	
+$config->db->password = 'wshile';		
+$config->db->prefix   = 'eps_';	
